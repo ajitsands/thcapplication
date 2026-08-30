7512aa8e9ab8bed76087a61e8f5d6c9bdec90deb
Ajit Kumar KV <ajitsands@gmail.com>
2026-08-30 15:30:59 +0530

    Fix file download issue: clean .htaccess handler and add proper PHP redirect

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<script>
-window.location.href = "view/login.php";
-</script>
+<?php
+header("Location: view/login.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="refresh" content="0;url=view/login.php">
+    <script>window.location.href = "view/login.php";</script>
+</head>
+<body>
+    <p>Redirecting to <a href="view/login.php">Login</a>...</p>
+</body>
+</html>
